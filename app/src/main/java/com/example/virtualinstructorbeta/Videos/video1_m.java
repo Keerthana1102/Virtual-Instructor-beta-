f08893917ac1ed312fcc26ca50bcedf28e8002b0
Siddharth-coder13 <sid021099kumar@gmail.com>
2020-07-07 20:07:53 +0530

    added test video in instructor

--- app/src/main/java/com/example/virtualinstructorbeta/Videos/video1_m.java
+++ app/src/main/java/com/example/virtualinstructorbeta/Videos/video1_m.java
@@ -2,15 +2,66 @@ package com.example.virtualinstructorbeta.Videos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.StatusBarManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.example.virtualinstructorbeta.R;
 
 public class video1_m extends AppCompatActivity {
 
+    private VideoView videoView;
+    private MediaController mediaController;
+    private String TAG;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video1_m);
+
+        getSupportActionBar().hide();
+
+        videoView = (VideoView) findViewById(R.id.video1);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Uri uri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/virtual-instructor.appspot.com/o/greenish_blue_butterfly_butterfly_523.mp4?alt=media&token=7e07a46b-41fe-45e9-bb9a-dfff808df3a0");
+
+
+        if(mediaController == null){
+            mediaController = new MediaController(this);
+            mediaController.setAnchorView(videoView);
+        }
+
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        //videoView.setLayoutParams(params);
+        videoView.start();
+
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(video1_m.this, "Completed", Toast.LENGTH_SHORT).show();
+            }
+        });
+        videoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                Toast.makeText(video1_m.this, "Some error occurred", Toast.LENGTH_SHORT).show();
+                Log.d(TAG,"not working");
+                return false;
+            }
+        });
+
     }
+
 }
--- app/src/main/java/com/example/virtualinstructorbeta/ui/tools/ToolsFragment.java
+++ app/src/main/java/com/example/virtualinstructorbeta/ui/tools/ToolsFragment.java
@@ -1,20 +1,22 @@
 package com.example.virtualinstructorbeta.ui.tools;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.virtualinstructorbeta.R;
+import com.example.virtualinstructorbeta.Videos.video1_m;
 
 public class ToolsFragment extends Fragment {
+
+    private CardView cardView;
 
     private ToolsViewModel toolsViewModel;
 
@@ -24,6 +26,25 @@ public class ToolsFragment extends Fragment {
                 ViewModelProviders.of(this).get(ToolsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_tools, container, false);
 
+
+
+        cardView = (CardView) root.findViewById(R.id.video1_m);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(), video1_m.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
+
+
         return root;
+
+
     }
 }
